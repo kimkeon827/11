4b5dbf3c05675968a09ffedcb6ef511937812a49
kimkeon827 <111333313+kimkeon827@users.noreply.github.com>
2022-08-16 14:54:50 +0900

    Console - Hello World

--- 01_console/01_console/Program.cs
+++ 01_console/01_console/Program.cs
@@ -10,6 +10,8 @@ namespace _01_console
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
